6176e5ea62cb31dd927b8db316e6b1711129588e
azhangd <87043902+azhangd@users.noreply.github.com>
2021-07-06 21:59:59 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,366 @@
+import json
+from urllib.request import urlopen
+
+import dash
+import dash_daq as daq
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+
+df = pd.read_csv('D:/sauce/trees/zip_zones.csv')
+df['min_temp'] = df['trange'].apply(lambda x: x.split(' ')[0])
+df['min_temp'] = pd.to_numeric(df['min_temp'])
+
+df_plants = pd.read_csv(
+'D:/sauce/trees/USDAPlants/usda_plants_20160223.csv', engine='python')
+
+df_plants.drop_duplicates()
+
+df_plants = df_plants.filter(
+    items=[
+        'Symbol',
+        'Scientific Name.x',
+        'Common Name'
+        'Species',
+        'Growth Habit',
+        'Temperature, Minimum (°F)',
+        'Bloom Period',
+        'Salinity Tolerance',
+        'Shade Tolerance'
+    ]
+)
+
+df_plants = df_plants.rename(
+    columns={
+        "Symbol": "symbol",
+        "Scientific Name.x": "scientific_name",
+        "Common Name": "common_name",
+        "Species": "species", "Growth Habit": "growth_habit",
+        "Temperature, Minimum (°F)": "min_temp",
+        "Bloom Period": "bloom_period",
+        "Salinity Tolerance": "salinity_tolerance",
+        "Shade Tolerance": "shade_tolerance",
+    }
+)
+
+df_plants = df_plants[df_plants['min_temp'].notnull()]
+
+px.set_mapbox_access_token("pk.eyJ1IjoiYmlnZG9nZGF0YSIsImEiOiJja3FiYnd2MWcwaDF1Mm9rZDhpNGVqc2gzIn0.XeBuYQMlGHgu4ml4R4RtRQ")
+
+colors = {
+    'background': 'rgba(0,0,0,0)',
+    'text': 'white'
+}
+
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# external_stylesheets = [dbc.themes.DARKLY]
+
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+
+theme =  {
+    'dark': True,
+    'detail': '#007439',
+    'primary': '#00EA64',
+    'secondary': '#6E6E6E',
+}
+
+
+app = dash.Dash(
+    __name__,
+    meta_tags=[
+            {
+                "name": "viewport",
+                "content": "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no",
+            }
+    ],
+)
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(html.Tr([html.Th(col) for col in dataframe.columns])),
+        html.Tbody([html.Tr([html.Td(dataframe.iloc[i][col]) for col in dataframe.columns])
+                   for i in range(min(len(dataframe), max_rows))])
+    ])
+
+
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~ APP LAYOUT ~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+
+app.layout = html.Div([
+    # html.H1(
+    #     children='Plant Hardiness Dashboard',
+    #     style={
+    #         'textAlign': 'center',
+    #         'color': 'white'
+    #     }
+    # ),
+
+    # html.Div(
+    #     children='Dash: A web application framework for Python.',
+    #     style={
+    #         'textAlign': 'center',
+    #         'color': colors['text']
+    #     }
+    # ),
+
+    html.Div(
+        [
+            html.Div(
+                [
+                    html.H1(
+                        "Plant Hardiness Dashboard",
+                        style={"margin-bottom": "0px"},
+                    )
+                ],
+                className="seven columns",
+                id="title"
+            ),
+            html.Div(
+                [
+                    html.A(
+                        html.Button("Source Code", id="learn-more-button"),
+                        href="https://plot.ly/dash/pricing/",
+                    )
+                ],
+                className="seven columns",
+                id="button"
+            )
+        ],
+        id="header",
+        className="row flex-display",
+        style={"margin-bottom": "25px"}
+    ),
+
+    html.Div([
+        html.Div([         
+            html.Label('Scientific Name'),
+
+            # daq.Thermometer( 
+            #     id='temperature',
+            #     value=0,
+            #     label="Temperature", 
+            #     max=65, 
+            #     min=-55, 
+            #     showCurrentValue=True, 
+            #     units="F", 
+            #     color='red'
+            # ),
+
+            dcc.Dropdown(
+                id='plants-dropdown',
+                options=[{'label': i, 'value': i} for i in df_plants['scientific_name']],
+                value=[],
+                multi=True
+                # style=dict(background='#1c2545')
+            ),
+
+            dcc.Graph(
+                id='plants-map',
+            ),
+
+            # dcc.RangeSlider(
+            #     id='my-range-slider',
+            #     min=-55,
+            #     max=65,
+            #     step=None,
+            #     marks={
+            #         -55: '-55 °F',
+            #         -50: '-50 °F',
+            #         -45: '-45 °F',
+            #         -40: '-40 °F',
+            #         -35: '-35 °F',
+            #         -30: '-30 °F',
+            #         -25: '-25 °F',
+            #         -20: '-20 °F',
+            #         -15: '-15 °F',
+            #         -10: '-10 °F',
+            #         -5: '-5 °F',
+            #         0: '0 °F',
+            #         5: '5 °F',
+            #         10: '10 °F',
+            #         15: '15 °F',
+            #         20: '20 °F',
+            #         25: '25 °F',
+            #         30: '30 °F',
+            #         35: '35 °F',
+            #         40: '40 °F',
+            #         45: '45 °F',
+            #         50: '50 °F',
+            #         55: '55 °F',
+            #         60: '60 °F',
+            #         65: '65 °F'
+
+            #     },
+            #     # allowCross=False,
+            #     disabled=True,
+            #     # vertical=True,
+            #     value=[-55, 65]
+            # ),
+
+        ], className="pretty_container seven columns"),
+
+    ], className="row"),
+
+    # html.Div(id='output-container-range-slider'),
+
+    html.Div(
+        [
+            dcc.Markdown(
+                """
+                **Click Data**
+                Click on points in the graph."""
+            ),
+
+            html.Pre(id='click-data', style=styles['pre']),
+
+            daq.LEDDisplay(
+                id='led-zipcode',
+                value="00000",
+                label='zipcode',
+                backgroundColor='transparent'
+            ),
+        ],
+        className="pretty_container seven columns"
+    )
+])
+
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~ APP METHODS ~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+
+
+def filter_df_plants(selected_plant):
+    df_plants_filtered = df_plants[df_plants['scientific_name'].isin(selected_plant)]
+    selected_temp = df_plants_filtered['min_temp'].max()
+    filtered_df = df[df['min_temp'] >= selected_temp]
+    return filtered_df
+# , df_plants_filtered
+
+
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~ APP CALLBACKS ~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+
+@app.callback(
+    Output('plants-map', 'figure'),
+    # Output('my-range-slider', 'options'),
+    #     Output('plants-table', 'figure'),
+    Input('plants-dropdown', 'value')
+)
+def update_graph(selected_plant):
+
+    if not selected_plant:
+        filtered_df = df
+    else:
+        filtered_df = filter_df_plants(selected_plant)
+#     , df_plants_filtered
+
+    fig = px.scatter_mapbox(
+        filtered_df,
+        lat="latitude",
+        lon="longitude",
+        hover_name="city",
+        hover_data=["zone", "zipcode"],
+        color="min_temp",
+        color_continuous_scale=px.colors.sequential.haline,
+        range_color=[-55, 65],
+        center={"lat": 37.0902, "lon": -95.7129},
+        zoom=4.5,
+        opacity=0.8
+    )
+
+    fig.update_layout(
+        legend_title_text='Min. Temp',
+        # mapbox_style="dark",
+        mapbox_style="mapbox://styles/bigdogdata/ckqreebqs3s2b17rygkmnnyhy/draft",
+        # mapbox_style="mapbox://styles/plotlymapbox/cjvppq1jl1ips1co3j12b9hex",
+
+        autosize=True,
+        uirevision='no reset of zoom',
+        margin=dict(l=30, r=30, b=20, t=0),
+        # margin={"r":10,"t":10,"l":10,"b":10},
+        #margin=dict(t=0, b=0, l=0, r=0),
+        height=1000,
+        #width = 1920,
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        legend_bgcolor=colors['background'],
+        font_color=colors['text']
+    )
+
+
+
+#     table_fig = dash_table.DataTable(
+#         columns=[{"name": i, "id": i} for i in df_plants_filtered.columns],
+#         data=df_plants_filtered,
+#         style_cell=dict(textAlign='left'),
+#         style_header=dict(backgroundColor="paleturquoise"),
+#         style_data=dict(backgroundColor="lavender")
+#     )
+
+    return fig
+# , table_fig
+
+
+@app.callback(
+    # Output('click-data', 'children'),
+    Output('led-zipcode', 'value'),
+    Input('plants-map', 'clickData'))
+def display_click_data(clickData):
+    if clickData is None:
+        return '00000'
+    else:
+        return clickData['points'][0]['customdata'][1]
+#     return json.dumps(clickData['points'][0]['customdata'], indent=2)
+
+@app.callback(
+    Output('click-data', 'children'),
+    # Output('led-zipcode', 'value'),
+    Input('plants-map', 'clickData'))
+def display_click_data(clickData):
+    if clickData is None:
+        return 'none'
+    else:
+        return json.dumps(clickData['points'][0]['customdata'], indent=2)
+
+# @app.callback(
+#     Output('temperature', 'value'),
+#     Input('plants-map', 'clickData'))
+# def display_click_data(clickData):
+#     if clickData is None:
+#         return 0
+#     else:
+#         click_zip = clickData['points'][0]['customdata'][1]
+#         print(df[['zipcode']==click_zip]['min_temp'])
+#         return df[['zipcode']==click_zip]['min_temp']
+
+
+# @app.callback(
+#     Output('temperature', 'color'),
+#     [Input('temperature', 'value')]
+# )
+# def update_therm_col(val):
+#     if val >= 0:
+#         return 'red'
+#     elif val < 0:
+#         return 'blue'
+
+
+app.run_server(debug=True, use_reloader=False)
