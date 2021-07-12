d9ccd87f9a6b307329348ea54ec2987c49740406
azhangd <azhang.data@gmail.com>
2021-07-11 18:51:34 -0700

    new cleaned data and improvements

--- app.py
+++ app.py
@@ -14,59 +14,33 @@ import plotly.express as px
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 
+# Get relative data paths
 this_directory = os.path.dirname(__file__)
 src_zip_zones = os.path.join(this_directory, 'data', 'zip_zones.csv')
-src_usda_plants = os.path.join(this_directory, 'data', 'usda_plants_20160223.csv')
+src_usda_plants = os.path.join(this_directory, 'data', 'usda_plants_filtered.csv')
 
+# Load data
 df = pd.read_csv(src_zip_zones)
+df_plants = pd.read_csv(src_usda_plants)
+
+# Get minimum temperature from trange
 df['min_temp'] = df['trange'].apply(lambda x: x.split(' ')[0])
 df['min_temp'] = pd.to_numeric(df['min_temp'])
 
-df_plants = pd.read_csv(src_usda_plants)
+# Fix column names
+df_plants.columns = df_plants.columns.str.lower()
 
-df_plants.drop_duplicates()
-df_plants['id'] = df_plants.index
+# Insert row id to df_plants
+df_plants.insert(loc=0, column='id', value=np.arange(len(df_plants)))
 
-df_plants = df_plants.filter(
-    items=[
-        'id',
-        'Symbol',
-        'Scientific Name.x',
-        'Common Name'
-        'Species',
-        'Growth Habit',
-        'Temperature, Minimum (°F)',
-        'Bloom Period',
-        'Salinity Tolerance',
-        'Shade Tolerance'
-    ]
-)
-
-df_plants = df_plants.rename(
-    columns={
-        "Symbol": "symbol",
-        "Scientific Name.x": "scientific_name",
-        "Common Name": "common_name",
-        "Species": "species", "Growth Habit": "growth_habit",
-        "Temperature, Minimum (°F)": "min_temp",
-        "Bloom Period": "bloom_period",
-        "Salinity Tolerance": "salinity_tolerance",
-        "Shade Tolerance": "shade_tolerance",
-    }
-)
-
-df_plants = df_plants[df_plants['min_temp'].notnull()]
-
-df_plants.head()
-
+# Set mapbox access token
 px.set_mapbox_access_token("pk.eyJ1IjoiYmlnZG9nZGF0YSIsImEiOiJja3FiYnd2MWcwaDF1Mm9rZDhpNGVqc2gzIn0.XeBuYQMlGHgu4ml4R4RtRQ")
 
+# Style settings
 colors = {
     'background': 'rgba(0,0,0,0)',
     'text': 'white'
 }
-
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 styles = {
     'pre': {
@@ -93,13 +67,10 @@ app = dash.Dash(
             }
     ],
 )
-
+app.title = 'Plants Lookup'
 server = app.server
 
-'''
-########################## APP LAYOUT ##########################
-'''
-
+# App layout
 app.layout = html.Div(
     [
         html.Div(
@@ -137,7 +108,7 @@ app.layout = html.Div(
 
                         dcc.Dropdown(
                             id='plants-dropdown',
-                            options=[{'label': i, 'value': i} for i in df_plants['scientific_name']],
+                            options=[{'label': i, 'value': i} for i in df_plants['common_name']],
                             value=[],
                             multi=True,
                             className="dcc_control"
@@ -158,61 +129,6 @@ app.layout = html.Div(
                             value="00000",
                             label='zipcode',
                             backgroundColor='transparent'
-                        ),
-
-                        html.Div(
-                            [
-                                # dash_table.DataTable(
-                                #     id='table-paging-and-sorting',
-                                #     columns=[
-                                #         {'name': i, 'id': i, 'deletable': True} for i in df_plants.columns
-                                #     ],
-                                #     page_current=0,
-                                #     page_size=PAGE_SIZE,
-                                #     page_action='custom',
-
-                                #     sort_action='custom',
-                                #     sort_mode='single',
-                                #     sort_by=[],
-                                #     style_data_conditional=[                
-                                #         {
-                                #             "if": {"state": "active"},  # 'active' | 'selected'
-                                #             "backgroundColor": "#525F89",
-                                #             "border": "#FFFFF"
-                                #             # "border": "1px solid blue"
-                                #         }
-                                #     ],
-                                #     style_cell={
-                                #         'backgroundColor': colors['background'],
-                                #         'color': colors['text'],
-                                #         'textAlign': 'right'
-                                #     },
-                                #     style_as_list_view=True,
-                                #     css=[
-                                #         {
-                                #             'selector': 'tr:hover', 
-                                #             'rule': 'background-color: #525F89;'
-                                #         }
-                                #     ],
-                                #     tooltip_data=[
-                                #         {
-                                #             column: {
-                                #                 'value': 'Location at Big Bear Lake\n\n![Big Bear Lake](https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Big_Bear_Valley%2C_California.jpg/1200px-Big_Bear_Valley%2C_California.jpg)', 
-                                #                 # 'value': 
-                                #                 'type': 'markdown'
-                                #                 }
-                                #             # column: {'value': get_image(value), 'type': 'markdown'}
-                                #             for column, value in row.items()
-                                #         } for row in df_plants.to_dict('records')
-                                #     ],
-                                # ),
-                                # dash_table.DataTable(
-                                #     id='output',
-                                #     columns=[
-                                #         {'name': i, 'id': i} for i in sorted(df_plants.columns)
-                                #     ],
-                                # )
-                            ],
                         ),
                         
                         # dcc.Markdown(
@@ -266,59 +182,38 @@ app.layout = html.Div(
                                     'rule': 'background-color: #525F89;'
                                 }
                             ],
-                            # tooltip_data=[
-                            #     {
-                            #         column: {
-                            #             'value': 'Location at Big Bear Lake\n\n![Big Bear Lake](https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Big_Bear_Valley%2C_California.jpg/1200px-Big_Bear_Valley%2C_California.jpg)', 
-                            #             # 'value': 
-                            #             'type': 'markdown'
-                            #             }
-                            #         # column: {'value': get_image(value), 'type': 'markdown'}
-                            #         for column, value in row.items()
-                            #     } for row in df_plants.to_dict('records')
-                            # ],
+                        ),
+
+                        html.Div(
+                            [
+                                html.Img(id="image-url")
+                            ],
                         ),
                     ], 
-                    className="pretty_container five columns"
+                    className="pretty_container"
                 ),
-
-                html.Div(
-                    [
-                        html.Img(id="image-url")
-                    ]
-                )
-
             ],
             className="row flex-display"
-        )
-
+        ),
     ],
     id="mainContainer",
     style={"display": "flex", "flex-direction": "column"}
 )
 
-'''
-########################## HELPER FUNCTIONS ##########################
-'''
-
+# Helper Functions
 def filter_df_plants(selected_plant):
-    df_plants_filtered = df_plants[df_plants['scientific_name'].isin(selected_plant)]
-    selected_temp = df_plants_filtered['min_temp'].max()
+    df_plants_filtered = df_plants[df_plants['common_name'].isin(selected_plant)]
+    selected_temp = df_plants_filtered['temperature_minimum_f'].max()
     filtered_df = df[df['min_temp'] >= selected_temp]
     return filtered_df
 
 def get_image_url(selected_plant):
     url_1 = 'https://plants.sc.egov.usda.gov/ImageLibrary/standard/'
     url_2 = '_001_svp.jpg'
-    symbol = df_plants[df_plants['scientific_name']==selected_plant]['symbol'].to_string(index=False)
+    symbol = df_plants[df_plants['common_name']==selected_plant]['symbol'].to_string(index=False)
     return(url_1 + symbol + url_2)
 
-                
-'''
-########################## CALLBACKS ##########################
-'''
-
-
+# Update map with dropdown
 @app.callback(
     Output('plants-map', 'figure'),
     Input('plants-dropdown', 'value')
@@ -361,19 +256,9 @@ def update_graph(selected_plant):
         legend_bgcolor=colors['background'],
         font_color=colors['text']
     )
-
-#     table_fig = dash_table.DataTable(
-#         columns=[{"name": i, "id": i} for i in df_plants_filtered.columns],
-#         data=df_plants_filtered,
-#         style_cell=dict(textAlign='left'),
-#         style_header=dict(backgroundColor="paleturquoise"),
-#         style_data=dict(backgroundColor="lavender")
-#     )
-
     return fig
-# , table_fig
 
-
+# Show zip code on map click
 @app.callback(
     Output('led-zipcode', 'value'),
     Input('plants-map', 'clickData'))
@@ -383,18 +268,8 @@ def display_click_data(clickData):
     else:
         return clickData['points'][0]['customdata'][1]
 
-'''
-# Scattermap click data
-'''
-# @app.callback(
-#     Output('click-data', 'children'),
-#     Input('plants-map', 'clickData'))
-# def display_click_data(clickData):
-#     if clickData is None:
-#         return 'none'
-#     else:
-#         return json.dumps(clickData['points'][0]['customdata'], indent=2)
 
+# Scattermap click data
 @app.callback(
     Output('table-paging-and-sorting', 'data'),
     Input('table-paging-and-sorting', "page_current"),
@@ -413,8 +288,8 @@ def update_table(page_current, page_size, sort_by):
 
     return df_plants_filtered.iloc[page_current*page_size:(page_current+ 1)*page_size].to_dict('records')
 
+# Show image on datatable click
 @app.callback(
-    # Output("output", "data"), 
     Output("image-url", "src"), 
     Input("table-paging-and-sorting", "active_cell")
 )
@@ -422,20 +297,8 @@ def cell_clicked(active_cell):
     if active_cell is None:
         return dash.no_update
 
-    row = active_cell["row_id"]
-    col = active_cell["column_id"]
-    selected_plant = df_plants.at[row, col]
-    # df_selected_plant= df_plants[df_plants['scientific_name'] == selected_plant],
-    # df_selected_plant= df_plants.query('scientific_name==@selected_plant')
-    # print(df_selected_plant)
-    # fig = dash_table.DataTable(
-    #     columns=[
-    #         {'name': i, 'id': i} for i in sorted(df_selected_plant.columns)
-    #     ],
-    #     data=df_selected_plant.to_dict("records"),
-    #     export_format="csv"
-    # )
-    # return fig
+    row_id = active_cell["row_id"]
+    selected_plant = df_plants.at[row_id, 'common_name']
     print(str(get_image_url(selected_plant)))
     return str(get_image_url(selected_plant))
 
